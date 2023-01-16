0b37a6588f70c9ddc8fcaad292a3344f82d62a6e
Jappananny <Jappananny@gmail.com>
2023-01-16 20:30:15 +0300

    Курсовая работа 3

---
+++ src/test/java/ru/service/socksapp/SocksAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.service.socksapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocksAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
